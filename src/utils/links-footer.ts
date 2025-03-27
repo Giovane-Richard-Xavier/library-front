02209f9dbe951dafc1ac4da52f40007cb9b4ded6
Giovane <giovane@rlvtecnologia.com.br>
2025-03-27 17:57:59 -0300

    feat(library): implementa versão inicial para o footer do site

---
+++ src/utils/links-footer.ts
@@ -0,0 +1,123 @@
+export const institucional = [
+  {
+    title: "Sobre nós",
+    href: "#",
+  },
+  {
+    title: "Direitos autorais",
+    href: "#",
+  },
+  {
+    title: "Programa de integridade",
+    href: "#",
+  },
+  {
+    title: "Quero ser vendedor",
+    href: "#",
+  },
+  {
+    title: "Imprensa",
+    href: "#",
+  },
+  {
+    title: "Blog",
+    href: "#",
+  },
+];
+
+export const ajuda = [
+  {
+    title: "Central de ajuda",
+    href: "#",
+  },
+  {
+    title: "Nossas garantias",
+    href: "#",
+  },
+  {
+    title: "Trocas e devoluções",
+    href: "#",
+  },
+  {
+    title: "Segurança e Privacidade",
+    href: "#",
+  },
+  {
+    title: "Política de entrega",
+    href: "#",
+  },
+  {
+    title: "Termos de uso",
+    href: "#",
+  },
+];
+
+export const navegue_col_1 = [
+  {
+    title: "+ Livros",
+    href: "#",
+  },
+  {
+    title: "Frete grátis",
+    href: "#",
+  },
+  {
+    title: "Sebos e Livrarias",
+    href: "#",
+  },
+  {
+    title: "Saldão de livros",
+    href: "#",
+  },
+  {
+    title: "Livros usados",
+    href: "#",
+  },
+  
+];
+
+export const navegue_col_2 = [
+  {
+    title: "Só aqui",
+    href: "#",
+  },
+  {
+    title: "Achados da estante",
+    href: "#",
+  },
+  {
+    title: "Livros mais vendidos",
+    href: "#",
+  },
+  {
+    title: "black friday",
+    href: "#",
+  },
+  {
+    title: "Auto ajuda",
+    href: "#",
+  },
+];
+
+export const navegue_col_3 = [
+  {
+    title: "Literatura brasileira",
+    href: "#",
+  },
+  {
+    title: "Livros de ficção e fantasia",
+    href: "#",
+  },
+  {
+    title: "Literatura infantil",
+    href: "#",
+  },
+  {
+    title: "Livros infanto-juvenis",
+    href: "#",
+  },
+  {
+    title: "Livros de terror",
+    href: "#",
+  },
+];
